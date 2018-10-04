f6ef6aa16f01709ef91611c4f85f8a2f0b680645
Aziz Haddad <aziz-haddad@users.noreply.github.com>
2018-10-04 11:10:01 +0200

    fix routing module

--- src/lib/products-routing.module.ts
+++ src/lib/products-routing.module.ts
@@ -2,10 +2,10 @@ import { Routes } from '@angular/router';
 import { RouterModule } from '@angular/router';
 import { ProductsComponent } from '@libs/products/src/lib/products.component';
 
-const contactsRoutes: Routes = [
+const productsRoutes: Routes = [
   {
     path: '', component: ProductsComponent
   }
 ];
 
-export const ProductsRoutingModule = RouterModule.forChild(contactsRoutes);
+export const ProductsRoutingModule = RouterModule.forChild(productsRoutes);
