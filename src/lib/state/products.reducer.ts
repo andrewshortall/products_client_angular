35a96d9dc70bbeb042d022cb2049d1473e333910
Aziz Haddad <aziz-haddad@users.noreply.github.com>
2018-11-20 15:01:14 +0100

    fix products reducer

--- src/lib/state/products.reducer.ts
+++ src/lib/state/products.reducer.ts
@@ -1,9 +1,7 @@
-import { MidgardState } from '@libs/midgard-angular/src/lib/state/midgard.model';
 import { LOAD_DATA_PRODUCTS_COMMIT } from '@libs/products/src/lib/state/products.actions';
 
-const initialState: MidgardState = {
-  workflowLevel1: [],
-  workflowLevel2: [],
+const initialState: any = {
+  products: [],
   dataLoaded: false
 };
 
@@ -11,7 +9,7 @@ export function productsReducer(state = initialState, action) {
   switch (action.type) {
     case LOAD_DATA_PRODUCTS_COMMIT:
       return Object.assign({}, state, {
-        workflowLevel1: action.data,
+        products: action.data,
         dataLoaded: true
       });
 
